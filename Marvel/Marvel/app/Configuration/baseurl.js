cee76c57e987198517805c55b57e65459f19a0f9
fabricio.palheta <fabricio.palheta@gmail.com>
2022-02-09 00:34:36 -0300

    projeto

--- Marvel/Marvel/app/Configuration/baseurl.js
+++ Marvel/Marvel/app/Configuration/baseurl.js
@@ -1,6 +1,6 @@
 angular.module("marvelBuscaApp").value("config", {
-    baseurl:" https://developer.marvel.com/docs",
+    baseurl:"http://gateway.marvel.com/",
     apipublickey:"5a237863b3cc2061003cbbc4fe20dc06",
-    apiprivatekey:" fbf255068eccea6d0ef951b9f25626b57ab2fe72",
+    apiprivatekey:"fbf255068eccea6d0ef951b9f25626b57ab2fe72",
     listagemCharacters:"/v1/public/characters"
 })
--- Marvel/Marvel/app/Services/marvelService.js
+++ Marvel/Marvel/app/Services/marvelService.js
@@ -1,7 +1,36 @@
 
-angular.module("marvelBuscaApp").service("marvelService" , function(config , $http){
-        this.characterlis = () => {
-            
-            return $http.get(apbaseurliba + listagemCharacters);
+
+angular.module("marvelBuscaApp").factory("marvelService" , function($http , $q){
+    return{
+       
+
+        GetTeste : function(url)
+        {
+            var deferred = $q.defer();
+            $http.get(url)
+                .success(function (response) {
+                    deferred.resolve(response);
+                })
+                .error(function (err, status) {
+                    deferred.reject(err);
+                });
+
+            return deferred.promise;
+        },
+
+        GetById : function(url)
+        {
+            $http.get(url)
+            .success(function (response) {
+                deferred.resolve(response);
+            })
+            .error(function (err, status) {
+                deferred.reject(err);
+            });
+
+            return deferred.promise;
         }
+
+       
+    }
 })
